07edd7366bcd1e00707589acde078f23c3024e3c
Muhammad-Bilal09 <156311521+Muhammad-Bilal09@users.noreply.github.com>
2025-09-15 01:49:19 -0700

    Update register.ts

--- src/pages/api/register.ts
+++ src/pages/api/register.ts
@@ -6,9 +6,9 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  if (req.method !== "POST") {
-    return res.status(405).json({ message: "Method not allowed" });
-  }
+  // if (req.method !== "POST") {
+  //   return res.status(405).json({ message: "Method not allowed" });
+  // }
 
   try {
     const { name, email, password } = req.body;
