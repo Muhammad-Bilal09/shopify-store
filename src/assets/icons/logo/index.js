1218a03f75980e0a193bda0cfa9fecea21582c00
mrbilalrana <bilal@gensols.org>
2026-07-20 12:01:28 +0500

    fix:"logo issue in mobile view"

--- src/assets/icons/logo/index.js
+++ src/assets/icons/logo/index.js
@@ -1,19 +1,13 @@
 import Image from "next/image";
 
-const Logo = () => {
+const Logo = ({ className = "" }) => {
   return (
     <Image
       src="/images/logo.png"
       alt="GreenOrganic Logo"
       width={90}
       height={90}
-      style={{
-        marginRight: "10px",
-        marginLeft: "20px",
-        borderRadius: "50%",
-        objectFit: "cover",
-        flexShrink: 0,
-      }}
+      className={`logo-image ${className}`}
       priority
     />
   );
--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -9,28 +9,28 @@ export const footerLinks = [
   {
     title: "Shopping online",
     links: [
-      { text: "Order Status", url: "#" },
-      { text: "Shipping and Delivery", url: "#" },
-      { text: "Returns", url: "#" },
-      { text: "Payment options", url: "#" },
-      { text: "Contact Us", url: "#" },
+      { text: "Order Status", url: "/contact" },
+      { text: "Shipping and Delivery", url: "/contact" },
+      { text: "Returns", url: "/contact" },
+      { text: "Payment options", url: "/contact" },
+      { text: "Contact Us", url: "/contact" },
     ],
   },
   {
     title: "Information",
     links: [
-      { text: "Gift Cards", url: "#" },
-      { text: "Find a store", url: "#" },
-      { text: "Newsletter", url: "#" },
-      { text: "Bacome a member", url: "#" },
-      { text: "Site feedback", url: "#" },
+      { text: "Gift Cards", url: "/products" },
+      { text: "Find a store", url: "/products" },
+      { text: "Newsletter", url: "/contact" },
+      { text: "Bacome a member", url: "/contact" },
+      { text: "Site feedback", url: "/contact" },
     ],
   },
   {
     title: "Contact",
     links: [
-      { text: "store@uikit.com", url: "#" },
-      { text: "Hotline: +1 131 138 138", url: "#" },
+      { text: "sajjadkhichi249@gmail.com", url: "mailto:sajjadkhichi249@gmail.com" },
+      { text: "Hotline: 0300-7864249", url: "tel:+923007864249" },
     ],
   },
 ];
